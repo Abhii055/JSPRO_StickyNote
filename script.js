6fcfb5d66f05faa0b47e004033c85a2992639b13
abhinav <abhinavdwivedi0807@gmail.com>
2024-05-27 11:02:42 +0530

    jspushed

---
+++ script.js
@@ -0,0 +1,63 @@
+const addBtn = document.querySelector('#addBtn');
+const main = document.querySelector('#main');
+
+
+addBtn.addEventListener("click",addNote);
+
+function addNote(){
+ const note = document.createElement("div");
+ note.classList.add("note");
+note.innerHTML = `
+            <div class="tool"> 
+            <i class="save fa-regular fa-save"></i>
+            <i class=" trash fa-regular fa-trash-can"></i></div>
+            <textarea id="textarea"></textarea>
+`;
+const trashIcon = note.querySelector(".trash");
+const saveIcon = note.querySelector(".save");
+const textArea = note.querySelector("textArea");
+
+trashIcon.addEventListener("click",()=>{
+    note.remove();
+    saveNote();
+})
+
+saveIcon.addEventListener("click",saveNote);
+textArea.addEventListener("input",saveNote);
+
+main.appendChild(note);
+}
+
+function saveNote(){
+    const notes =document.querySelectorAll(".note textarea");
+    const data =[];
+    for(let i =0;i<notes.length;i++)
+    {
+        data.push(notes[i].value);
+    }
+    if(data.length === 0){
+        localStorage.removeItem("notes")
+    }
+    else{
+        localStorage.setItem("notes",JSON.stringify(data));
+    }
+
+}
+
+function loadNotes(){
+    const lsNotes =JSON.parse(localStorage.getItem("notes"));
+
+    if(lsNotes!== null){
+        lsNotes.forEach(noteText => {
+            addNote();
+            const notes = document.querySelectorAll(".note textarea");
+            const lastNote = notes[notes.length-1];
+            lastNote.value =noteText;
+
+        });
+    }
+    else{
+        addNote();
+    }
+}
+loadNotes();
